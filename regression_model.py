b75d764e9971174d07a6afd2d3e59adee41c144b
Layne Malek <lmalek@umich.edu>
2025-08-11 13:59:54 -0400

    Added requirements file and continued working on model/evaluation metrics/plots

--- regression_model.py
+++ regression_model.py
@@ -15,7 +15,7 @@ from sklearn.calibration import CalibratedClassifierCV
 
 
 #Schedule for project:
-# 1. Introduction and python/numpy basics
+# 1. Introduction and setup and numpy/python basics
 # 2. Data loading and exploration
 # 3. Data preprocessing and feature engineering
 # 4. Model 1 training
@@ -61,7 +61,7 @@ def train_model(X: npt.NDArray, y: npt.NDArray):
     return model
 
 #Function to evaluate the trained model
-def evaluate_model(model, X: npt.NDArray, y: npt.NDArray):
+def evaluate_model(model, X: npt.NDArray, y_true: npt.NDArray):
     """
     Parameters:
     model: Trained logistic regression model.
@@ -71,20 +71,37 @@ def evaluate_model(model, X: npt.NDArray, y: npt.NDArray):
     Returns:
     dict: Evaluation metrics.
     """
-    # Predict probabilities
+    #Predict probabilities
     y_pred = model.predict_proba(X)[:, 1]
     
-    # Calculate ROC AUC score
-    roc_auc = metrics.roc_auc_score(y, y_pred)
+    #Calculate AUROC score
+    auroc = metrics.roc_auc_score(y_true, y_pred, labels = [-1,1])
     
-    # Calculate accuracy
-    accuracy = metrics.accuracy_score(y, model.predict(X))
+    #Calculate accuracy
+    accuracy = metrics.accuracy_score(y_true, y_pred)
 
-    # Calculate precision
-
-    # Calculate other metrics
+    #Calculate precision
+    precision = metrics.precision_score(y_true, y_pred, zero_division = 0.0)
     
-    return {'roc_auc': roc_auc, 'accuracy': accuracy}
+    #Calculate recall
+    recall = metrics.recall_score(y_true,y_pred)
+
+    #Calculate F1 score
+    f1_score = metrics.f1_score(y_true, y_pred)
+
+    #Calculate specificity
+    conf_matrix = metrics.confusion_matrix(y_true, y_pred)
+    denom = conf_matrix[0][0]+conf_matrix[0][1]
+    if denom == 0:
+        denom = 1
+    specificity = conf_matrix[0][0]/(denom)
+    
+    #Calculate average precision
+    average_precision = metrics.average_precision_score(y_true, y_pred)
+   
+    
+    return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1_score': f1_score, 'specificity': specificity, 
+            'average_precision': average_precision, 'auroc': auroc}
 
 def visualize_results(y_true: npt.NDArray, y_pred: npt.NDArray):
     """
@@ -95,8 +112,23 @@ def visualize_results(y_true: npt.NDArray, y_pred: npt.NDArray):
     Returns:
     None: Displays the ROC curve and confusion matrix.
     """
+    
     # Plot ROC curve
 
+    plt.figure(figsize=(7, 5))
+
+    
+    #fpr, tpr, _ = roc_curve(test_df['True'], test_df[model])
+    #roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, label=f'{model} (AUC = {roc_auc:.2f})')
+
+    plt.plot([0, 1], [0, 1], 'r--', label='Random Guess')
+
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend()
+    plt.show()
     # Plot confusion matrix
 
     #
